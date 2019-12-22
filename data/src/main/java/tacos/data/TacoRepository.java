d363bdeb98f4acb2c1fcead970519d87c3f204a1
xpyct707 <xpyct707@gmail.com>
2019-12-22 20:12:06 +0400

    Made Spring Data REST available. Did till 6.3.2.

--- data/src/main/java/tacos/data/TacoRepository.java
+++ data/src/main/java/tacos/data/TacoRepository.java
@@ -1,7 +1,9 @@
 package tacos.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import tacos.Taco;
 
+@CrossOrigin(origins = "*")
 public interface TacoRepository extends JpaRepository<Taco, Long> {
 }
